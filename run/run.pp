bbf6b0b6bde004e59dd7b797cfb36e6cba39b36d
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2025-04-24 16:03:09 -0400

    Initial commit for river

---
+++ run/run.pp
@@ -0,0 +1,33 @@
+#!/bin/tcsh
+set pp_input_files = $1
+set pp_exec_path = $2
+set pp_out_dir = $3
+
+mkdir -p scratch
+cd scratch
+
+echo $#pp_input_files > fort.5
+foreach file ($pp_input_files)
+   gcp $file .
+   echo $file:t >> fort.5
+end
+echo ""
+echo RUN POST-PROCESSOR
+cat fort.5
+${pp_exec_path}/cp_rvar_tmp < fort.5
+#cp_river_vars < fort.5
+
+if ($status != 0) then
+    echo ERROR in post-processing, exiting...
+    exit
+else
+    mv *.nc fort.* out.* ${pp_out_dir}/
+endif
+
+
+cd ../
+rm -rf scratch
+
+unset pp_out_dir
+unset pp_exec_path
+unset pp_input_files
